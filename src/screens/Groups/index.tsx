a0335585891add73bc251b6874d83a6201a26976
thgMatajs <tprmatajs@gmail.com>
2023-03-14 19:34:38 -0300

    add flat list to linsting groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,19 +1,34 @@
+import { useState } from 'react';
+
 import { Header } from '../../components/Header';
 import { HighLight } from '../../components/HighLight';
 import { GroupCard } from '../../components/GroupCard';
 
-
 import { Container } from './styles';
+import { FlatList } from 'react-native';
 
 
 export function Groups() {
+    const [groups, setGroups] =
+        useState<string[]>(['Galera da Gentalha', 'Galera do Madruga']);
+
     return (
         <Container>
             <Header />
+
             <HighLight
                 title='Dominando a linguagem'
                 subTitle='mataJS' />
-            <GroupCard title='Galera do brilho' />
+
+            <FlatList
+                data={groups}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                    <GroupCard
+                        title={item}
+                    />
+                )}
+            />
         </Container>
     );
 }
