95266221e038f5cdcb0774ccf2d3910d44305ada
thgMatajs <tprmatajs@gmail.com>
2023-03-14 16:49:09 -0300

    create theme and implement styled components

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,12 @@
+import { Container, Title } from './styles';
+
+
+export function Groups() {
+    return (
+        <Container>
+            <Title>
+                TeamApp
+            </Title>
+        </Container>
+    );
+}
