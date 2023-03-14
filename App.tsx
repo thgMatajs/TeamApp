781588809dd8dee180ce19c19b3c8d5aee11a497
thgMatajs <tprmatajs@gmail.com>
2023-03-14 17:36:50 -0300

    implement customs fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
 import { ThemeProvider } from 'styled-components';
-import { Groups } from './src/screens/Groups';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
 import theme from './src/theme';
 
+import { Groups } from './src/screens/Groups';
+import { ActivityIndicator } from 'react-native';
+
+
 export default function App() {
+
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   )
 }
