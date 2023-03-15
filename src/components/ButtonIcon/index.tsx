9b691dad2a03e7d9f511d5d5a7ec1cc4c72d9b47
thgMatajs <tprmatajs@gmail.com>
2023-03-14 23:28:00 -0300

    create and implement button icon

---
+++ src/components/ButtonIcon/index.tsx
@@ -0,0 +1,20 @@
+import { TouchableOpacityProps } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons"
+
+import { ButtonIconTypeProps, Container, Icon } from "./styles";
+
+type Props = TouchableOpacityProps & {
+    icon: keyof typeof MaterialIcons.glyphMap
+    type?: ButtonIconTypeProps
+}
+
+export function ButtonIcon({ icon, type = 'PRIMARY', ...rest }: Props) {
+    return (
+        <Container {...rest}>
+            <Icon
+                name={icon}
+                type={type}
+            />
+        </Container>
+    )
+}
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,6 +1,8 @@
+import { ButtonIcon } from "../../components/ButtonIcon";
 import { Header } from "../../components/Header";
 import { HighLight } from "../../components/HighLight";
-import { Container } from "./styles";
+import { Input } from "../../components/Input";
+import { Container, Form } from "./styles";
 
 export function Players() {
     return (
@@ -12,6 +14,18 @@ export function Players() {
                 title="Nome da turma"
                 subTitle="Adicione a galera e separe os times"
             />
+
+            <Form>
+                <Input
+                    placeholder="Nome da participante"
+                    autoCorrect={false}
+                />
+
+                <ButtonIcon
+                    icon="add"
+                />
+            </Form>
+
         </Container>
     )
 }
