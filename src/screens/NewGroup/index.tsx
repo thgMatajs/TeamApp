40f639f274b069c5168efaa569eca229efef6385
thgMatajs <tprmatajs@gmail.com>
2023-03-14 22:30:57 -0300

    create and implement input component

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,6 +1,7 @@
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 import { HighLight } from "../../components/HighLight";
+import { Input } from "../../components/Input";
 import { Container, Content, Icon } from "./styles";
 
 export function NewGroup() {
@@ -18,8 +19,12 @@ export function NewGroup() {
                     subTitle="Criar um novo grupo"
                 />
 
+                <Input
+                    placeholder="Nome da turma" />
+
                 <Button
                     title="Criar novo Grupo"
+                    style={{ marginTop: 16 }}
                     onPress={() => { }}
                 />
 
