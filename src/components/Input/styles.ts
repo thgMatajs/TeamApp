eb1c195c510c2bc31c8516dcdd253ba5f96ac221
thgMatajs <tprmatajs@gmail.com>
2023-03-14 22:50:30 -0300

    refactor to use css helper

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components/native"
+import styled, { css } from "styled-components/native"
 import { TextInput } from "react-native"
 
 export const Container = styled(TextInput)`
@@ -10,9 +10,11 @@ max-height: 56px;
 border-radius: 6px;
 padding: 16px;
 
-background-color: ${({ theme }) => theme.COLORS.GRAY_700};
-color: ${({ theme }) => theme.COLORS.WHITE};
+${({ theme }) => css`
+background-color: ${theme.COLORS.GRAY_700};
+color: ${theme.COLORS.WHITE};
 
-font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
-font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
+font-family: ${theme.FONT_FAMILY.REGULAR};
+font-size: ${theme.FONT_SIZE.MD}px;
+`}
 `
