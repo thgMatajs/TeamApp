0dfdb4df883956d894a79796126c8ce9ff327d08
thgMatajs <tprmatajs@gmail.com>
2023-03-15 00:02:33 -0300

    adjusts in filter component

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
+
+import { FlatList } from "react-native";
 import { ButtonIcon } from "../../components/ButtonIcon";
+import { Filter } from "../../components/Filter";
 import { Header } from "../../components/Header";
 import { HighLight } from "../../components/HighLight";
 import { Input } from "../../components/Input";
-import { Container, Form } from "./styles";
+import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 
 export function Players() {
+    const [team, setTeam] = useState('Time a')
+    const [players, setPlayers] = useState([])
     return (
         <Container>
 
@@ -25,6 +31,25 @@ export function Players() {
                     icon="add"
                 />
             </Form>
+
+            <HeaderList>
+                <FlatList
+                    data={["Time a", "Time b"]}
+                    keyExtractor={item => item}
+                    renderItem={({ item }) => (
+                        <Filter
+                            title={item}
+                            isActive={item === team}
+                            onPress={() => setTeam(item)}
+                        />
+                    )}
+                    horizontal
+                />
+
+                <NumberOfPlayers>
+                    {players.length}
+                </NumberOfPlayers>
+            </HeaderList>
 
         </Container>
     )
