9aabf399f54df5cea3fa2ce068a7c933564fac9d
thgMatajs <tprmatajs@gmail.com>
2023-03-15 23:32:25 -0300

    create and implement player card

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -6,6 +6,8 @@ import { Filter } from "../../components/Filter";
 import { Header } from "../../components/Header";
 import { HighLight } from "../../components/HighLight";
 import { Input } from "../../components/Input";
+import { ListEmpty } from "../../components/ListEmpty";
+import { Playercard } from "../../components/PlayerCard";
 import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 
 export function Players() {
@@ -50,6 +52,27 @@ export function Players() {
                     {players.length}
                 </NumberOfPlayers>
             </HeaderList>
+
+            <FlatList
+                data={players}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                    <Playercard
+                        name={item}
+                        onRemove={() => { }}
+                    />
+                )}
+                showsVerticalScrollIndicator={false}
+                ListEmptyComponent={() => (
+                    <ListEmpty
+                        message="Que tal adicionar um jogador?"
+                    />
+                )}
+                contentContainerStyle={[
+                    { paddingBottom: 56 },
+                    players.length === 0 && { flex: 1 }
+                ]}
+            />
 
         </Container>
     )
